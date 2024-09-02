18f1e1118e62f82379523d6a3213d62fb38913ba
Antonio Vuono <antoniosvuono@icloud.com>
2024-09-02 17:49:22 -0300

    feature: colors theme configured

---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    GRAY_7: '#F7F7F8',
+    GRAY_6: '#EDECEE',
+    GRAY_5: '#D9D8DA',
+    GRAY_4: '#9F9BA1',
+    GRAY_3: '#5F5B62',
+    GRAY_2: '#3E3A40',
+    GRAY_1: '#1A181B',
+    BLUE: '#364D9D',
+    BLUE_LIGHT: '#647AC7',
+  },
+}
