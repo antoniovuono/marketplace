be11ff02a7124fdff0405a4a178c593fd5bf9fc8
Antonio Vuono <antoniosvuono@icloud.com>
2024-09-03 20:26:07 -0300

    chore: svg transformer configured on project

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import { Container, Title } from './styles'
+import Logotype from '@assets/logotype.svg'
 
 export function SignIn() {
   return (
     <Container>
       <Title>Sign In</Title>
+      <Logotype />
     </Container>
   )
 }
