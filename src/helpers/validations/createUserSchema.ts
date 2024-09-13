0408b397d0f0fe7dfa0f4dc46ae603cbc423c1a7
Antonio Vuono <antoniosvuono@icloud.com>
2024-09-12 22:48:12 -0300

    feature: password does not match validation

--- src/helpers/validations/createUserSchema.ts
+++ src/helpers/validations/createUserSchema.ts
@@ -1,25 +1,34 @@
 import { z } from 'zod'
 
-export const createUserSchema = z.object({
-  name: z.string({
-    required_error: 'Nome é obrigatório',
-  }),
-  email: z
-    .string({
-      required_error: 'E-mail é obrigatório',
-    })
-    .email({
-      message: 'E-mail inválido',
+export const createUserSchema = z
+  .object({
+    name: z.string({
+      required_error: 'Nome é obrigatório',
     }),
-  phone: z.string({
-    required_error: 'Telefone é obrigatório',
-  }),
-  password: z.string({
-    required_error: 'Senha é obrigatória',
-  }),
-  confirmPassword: z.string({
-    required_error: 'Confirmação de senha é obrigatória',
-  }),
-})
+    email: z
+      .string({
+        required_error: 'E-mail é obrigatório',
+      })
+      .email({
+        message: 'E-mail inválido',
+      }),
+    phone: z.string({
+      required_error: 'Telefone é obrigatório',
+    }),
+    password: z
+      .string({
+        required_error: 'Senha é obrigatória',
+      })
+      .min(8, 'A senha deve ter no mínimo 8 caracteres'),
+    confirmPassword: z
+      .string({
+        required_error: 'Confirmação de senha é obrigatória',
+      })
+      .min(8, 'A senha deve ter no mínimo 8 caracteres'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword'],
+  })
 
 export type createUserSchemaType = z.infer<typeof createUserSchema>
